b7a3c1931665cae2127a834ea9b05b871b567731
DevIsha-a <EishaTabassum0@gmail.com>
2024-04-30 17:54:45 +0500

    added syncfusion

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DashboardLayout from './DashboardLayout'
+import Orders from './Orders'
 
 const Dashboard = () => {
     return (
         <DashboardLayout>
             <div className='db_comp '>
+                <Orders />
                 <div className="custom_card  sec_text">
                     Hello I am a card
                 </div>
